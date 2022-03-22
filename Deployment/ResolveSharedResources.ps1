7db2632906141d80e9ce6a40097180662c607cf0
David Lee <leedavid@microsoft.com>
2022-03-22 12:17:18 -0500

    reference rg

--- Deployment/ResolveSharedResources.ps1
+++ Deployment/ResolveSharedResources.ps1
@@ -3,5 +3,5 @@ param(
 
 # This is the rg where the VNETs should be deployed
 $groups = az group list --tag stack-environment=$BUILD_ENV | ConvertFrom-Json
-$networkingResourceGroup = ($groups | Where-Object { $_.tags.'stack-name' -eq 'platform' -and $_.tags.'stack-environment' -eq $BUILD_ENV }).name
+$networkingResourceGroup = ($groups | Where-Object { $_.tags.'stack-name' -eq 'platform' -and $_.tags.'stack-environment' -eq $BUILD_ENV -and $_.tags.'stack-sub-name' -eq 'networking' }).name
 Write-Host "::set-output name=resourceGroup::$networkingResourceGroup"
